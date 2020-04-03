e63cd785177169db9c15683a68a5ad1f42aec796
Jitgitbit <rowowolf@gmail.com>
2020-04-03 12:58:26 +0200

    router-link :to   The power of Vue.js !

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+    <NavBar/>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    NavBar,
+  }
 }
 </script>
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li><router-link to='/'>Home</router-link></li>
+      <li><router-link :to="{name:'About'}">About</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name:'NavBar',
+  data(){
+    return{}
+  }
+}
+</script>
+
+<style scoped>
+  ul{
+    list-style-type: none;
+    padding: 0;
+  }
+  a{  
+    /* in Vue.js when it sees the router-link, it interprets it as an a         */
+    color: #42b983;
+  }
+</style>
