4b2b0b0410ae372ca72c441353f2afd79629d31b
Jitgitbit <rowowolf@gmail.com>
2020-04-03 12:39:51 +0200

    basic About created, and routed !

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import About from '@/components/About'                   // the @ there means go to the root of the project !!
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    }
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
   ]
 })
